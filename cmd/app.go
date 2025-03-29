57f7bce1c9dfa1e2c0e7c5625444e7f731774b79
Yahor Kuzaukou <egkuzoff@gmail.com>
2025-03-29 20:59:58 +0300

    add jwt libs and basic config

--- cmd/app.go
+++ cmd/app.go
@@ -1,12 +1,22 @@
 package cmd
 
 import (
+	jwtware "github.com/gofiber/contrib/jwt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/yahorkuzaukou/loyalty-app/pkg/api/handlers"
 )
 
 func NewApp() *fiber.App {
 	app := fiber.New()
+	app.Use(
+		jwtware.New(
+			jwtware.Config{
+				SigningKey: jwtware.SigningKey{
+					Key: []byte("somesecret"),
+				},
+			},
+		),
+	)
 
 	apiGroup := app.Group("/api")
 	apiGroup.Group("/health", handlers.GetHealthHandlers()...)
